d3466f50f681d2fbd2ec68ee29b8668fec6f51d0
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-01-26 14:49:23 +0100

    Test jQuery from 1.11.1 to 3.0.0

--- test/javascript/server.rb
+++ test/javascript/server.rb
@@ -31,7 +31,7 @@ end
 use AssetPath, urls: ['/vendor/assets/javascripts'], root: rails_validations_path.full_gem_path
 use AssetPath, urls: ['/vendor/assets/javascripts'], root: File.expand_path('../..', settings.root)
 
-JQUERY_VERSIONS = %w(1.7.0 1.7.1 1.7.2 1.8.0 1.8.1 1.8.2 1.8.3 1.9.0 1.9.1 1.10.0 1.10.1 1.10.2 1.11.0 1.11.1 1.11.2 1.11.3 1.12.0 1.12.1 1.12.2 1.12.3 1.12.4).freeze
+JQUERY_VERSIONS = %w(1.11.1 1.11.2 1.11.3 1.12.0 1.12.1 1.12.2 1.12.3 1.12.4 2.1.1 2.1.2 2.1.3 2.1.4 2.2.0 2.2.1 2.2.2 2.2.3 2.2.4 3.0.0).freeze
 
 helpers do
   def jquery_link(version)
